97e659d9f77da12144b32e6ab48ce02f35328a89
pedroperalva <pedrobeltraoperalva@gmail.com>
2025-07-24 20:15:03 -0300

    fix: remove testimonials on menuItems

--- src/app/utils/menuItems.ts
+++ src/app/utils/menuItems.ts
@@ -2,7 +2,7 @@ export const menuItems = [
   { label: "services", href: "#servicos" },
   { label: "about", href: "#about" },
   { label: "portfolio", href: "#realizacoes" },
-  { label: "testimonials", href: "#testimonials" },
+  // { label: "testimonials", href: "#testimonials" },
   { label: "tendency", href: "#tendency" },
   { label: "contact", href: "#contact" },
   // { label: "ASSESSORIA", href: "/assessoria" },
