75dce469e5fb94c9e57b916da8036ffb6de3c4b5
pedroperalva <pedrobeltraoperalva@gmail.com>
2025-08-19 13:50:33 -0300

    feat: gtm scripts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import { Poiret_One } from "next/font/google";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
+import Script from "next/script"; // 👈 import Script
 
 const poiret = Poiret_One({
   weight: "400",
@@ -32,9 +33,32 @@ export default async function RootLayout({
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
+
   return (
     <html lang={locale}>
+      <head>
+        {/* ✅ Google Tag Manager Script */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W3JC23DQ');
+          `}
+        </Script>
+      </head>
       <body className={`${poiret.className} bg-[var(--main-bg)]`}>
+        {/* ✅ Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W3JC23DQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <NextIntlClientProvider>
           <Navbar />
           {children}
